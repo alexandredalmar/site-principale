e59d374bfca8611ed2704fb859786d42e0a3a711
GareFunkle <vincent.durret@gmail.com>
2024-07-21 14:28:52 +0200

    update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,38 +3,74 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className='flex h-auto flex-row items-center justify-around bg-slate-500'>
-      <div className='flex h-24 items-center'>
-        <h1>LOGO</h1>
+    <header className='navbar bg-base-100'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className='menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow'
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            {/* <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li> */}
+            <li>
+              <a>Item 2</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className='btn btn-ghost text-xl'>daisyUI</a>
       </div>
-      <nav>
-        <ul className='flex flex-row gap-8'>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1'>
           <li>
-            <Link
-              className='rounded-sm bg-orange-500 p-2 text-white transition-all hover:bg-orange-300'
-              href='#'
-            >
-              Accueil
-            </Link>
+            <a>Item 1</a>
+          </li>
+          {/* <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li> */}
+          <li>
+            <a>Item 2</a>
           </li>
           <li>
-            <Link
-              className='rounded-sm bg-orange-500 p-2 text-white transition-all hover:bg-orange-300'
-              href='#'
-            >
-              Nos prestations
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='rounded-sm bg-orange-500 p-2 text-white transition-all hover:bg-orange-300'
-              href='#'
-            >
-              Contact
-            </Link>
+            <a>Item 3</a>
           </li>
         </ul>
-      </nav>
+      </div>
+      <div className='navbar-end'>
+        <a className='btn'>TEL</a>
+      </div>
     </header>
   );
 };
