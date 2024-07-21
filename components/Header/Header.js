9a2272bfcb9788c097c512c28f1e7507bffbb0a6
GareFunkle <vincent.durret@gmail.com>
2024-07-21 12:14:57 +0200

    ajout du composant Header et du Footer

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-24 bg-red-500 flex items-center justify-center">
+      Header
+    </div>
+  );
+};
+
+export default Header;
