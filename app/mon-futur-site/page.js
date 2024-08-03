abb7ad21e6f268b85420457a565984b1c06a767b
GareFunkle <vincent.durret@gmail.com>
2024-08-03 14:16:03 +0200

    ajout des package manquand

---
+++ app/mon-futur-site/page.js
@@ -0,0 +1,8 @@
+import FuturSitePage from "@/views/futur-site";
+import React from "react";
+
+const FuturSite = () => {
+  return <FuturSitePage />;
+};
+
+export default FuturSite;
---
+++ views/futur-site.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FuturSitePage = () => {
+  return <div>FuturSitePage</div>;
+};
+
+export default FuturSitePage;
