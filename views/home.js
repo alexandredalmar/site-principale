9a854a83ed228c9d3aa17117fae56e1aee0971a5
alexandre dalmar <alexandredalmar@iMac-de-alexandre.local>
2024-10-26 22:21:41 +0200

    Mise à jour Digi house

--- views/home.js
+++ views/home.js
@@ -1,27 +1,31 @@
+"use client";
+
 import SectionsHomeBannerVideo from "@/components/Sections/SectionsHome/SectionsHomeBanner/SectionsHomeBannerVideo";
 import SectionHomeGrid from "@/components/Sections/SectionsHome/SectionsHomeGrid/SectionHomeGrid";
 import SectionHomeIntro from "@/components/Sections/SectionsHome/SectionsHomeIntro/SectionHomeIntro";
-import Button from "@/components/ui/button/Button";
-import Grid from "@/components/ui/Grid";
 import React from "react";
+import { motion } from "framer-motion";
+
+const pageVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 5 } },
+
+};
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-      <div className='wrapper'>
-        <div className='mainContainer'>
-          <SectionsHomeBannerVideo />
-
-          <SectionHomeIntro />
-
-          <SectionHomeGrid />
-
-          <h3 className='w-full py-16 text-center text-5xl sm:text-6xl'>
-            Ils nous ont fait confiance
-          </h3>
-        </div>
-      </div>
-    </div>
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={pageVariants}
+    >
+      <SectionsHomeBannerVideo />
+      <SectionHomeIntro />
+      <SectionHomeGrid />
+      <h3 className='w-full py-16 text-center text-5xl sm:text-6xl'>
+        Ils nous ont fait confiance
+      </h3>
+    </motion.div>
   );
 };
 
