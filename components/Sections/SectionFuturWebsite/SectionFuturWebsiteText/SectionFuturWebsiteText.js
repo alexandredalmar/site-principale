1ea25f44ecb70ef6b0114a52526cee44638dd357
GareFunkle <vincent.durret@gmail.com>
2024-10-29 21:54:29 +0100

    refresh folder with new library

---
+++ components/Sections/SectionFuturWebsite/SectionFuturWebsiteText/SectionFuturWebsiteText.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const SectionFuturWebsiteText = () => {
+  return (
+    <div className='mx-auto max-w-[1244px]'>
+      <h2 className='my-8 text-3xl'>
+        Découvrez notre nouvel outil de personnalisation : Visualisez votre logo
+        sur nos templates !
+      </h2>
+
+      <p className='text-lg'>
+        Nous sommes ravis de vous présenter notre tout nouvel outil en ligne,
+        conçu pour vous offrir une expérience de personnalisation unique. Grâce
+        à cet outil, vous pouvez désormais ajouter votre propre logo directement
+        sur nos templates et visualiser instantanément le rendu avec nos
+        couleurs et designs.
+      </p>
+
+      <h3 className='my-6 text-2xl'>Comment ça marche ?</h3>
+
+      <ul className='flex flex-col gap-4 text-lg'>
+        <li>
+          - Téléchargez votre logo : Choisissez l'image de votre logo que vous
+          souhaitez intégrer.
+        </li>
+        <li>
+          - Prévisualisez : Une fois votre logo téléchargé, il sera
+          automatiquement appliqué sur nos templates. Vous pourrez voir en temps
+          réel comment il s'intègre à nos différentes options de design.
+        </li>
+        <li>
+          - Explorez les possibilités : Parcourez nos différentes mises en page
+          et combinaisons de couleurs pour trouver celle qui correspond le mieux
+          à l’identité visuelle de votre entreprise.
+        </li>
+      </ul>
+
+      <p className='my-6 text-lg'>
+        Cet outil est idéal pour vous donner une idée précise de l'apparence de
+        vos supports marketing, produits ou tout autre élément personnalisé avec
+        notre service. Plus besoin d’imaginer le résultat final – vous pouvez le
+        voir immédiatement !
+      </p>
+
+      <h3 className='my-6 text-2xl'>
+        Pourquoi utiliser notre outil de prévisualisation ?
+      </h3>
+
+      <ul className='flex flex-col gap-4 text-lg'>
+        <li>
+          - Gain de temps : Inutile de faire des allers-retours avec notre
+          équipe pour visualiser des échantillons.
+        </li>
+        <li>
+          - Décisions éclairées : Voyez exactement ce que vous allez obtenir
+          avant de passer commande.
+        </li>
+        <li>
+          - Personnalisation optimale : Testez plusieurs combinaisons jusqu’à ce
+          que vous trouviez celle qui correspond parfaitement à votre vision.
+        </li>
+      </ul>
+
+      <p className='my-6 text-lg'>
+        Essayez notre outil dès maintenant et assurez-vous que chaque détail
+        reflète parfaitement votre marque !
+      </p>
+    </div>
+  );
+};
+
+export default SectionFuturWebsiteText;
