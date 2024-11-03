bd4ca384b0d5cb710b3e5311ad4c27c290e96bbd
alexandredalmar <alexandredalmar@gmail.com>
2024-11-03 14:26:16 +0100

    Mise à jour nos model

--- components/Sections/SectionsHome/SectionsHomeGrid/SectionHomeGrid.js
+++ components/Sections/SectionsHome/SectionsHomeGrid/SectionHomeGrid.js
@@ -3,30 +3,26 @@
 import Grid from "@/components/ui/Grid";
 import React from "react";
 import { motion } from "framer-motion";
-import  Link  from "next/link"
+import Link from "next/link";
 
 const items = [
   {
-    src: "images/nos-modeles/GreenMoverz-model.png",
+    src: "/img-exemple.png",
     title: "Nos créations",
     text: "Digi House crée des sites web dans le style qui vous correspond",
-    btn: "découvrir",
-    link: "/nos-modeles",
-
-  },
-  {
-    src: "images/nos-modeles/impact-model.png",
-    title: "Entretenir votre site",
-    text: "Nous vous proposons de vous aider à faire evoluer votre site en prenant la main",
     btn: "En savoir +",
-    link: "En savoir +",
   },
   {
-    src: "images/nos-modeles/lecompte-architecture-model.png",
-    title: "Nous contacter",
-    text: "Pour faire connaissance et nous faire part de votre projet.",
-    btn: "contact",
-    link: "/contact",
+    src: "/img-exemple.png",
+    title: "Agence LLinares Immobilier 2",
+    text: "Créateur de la Maison LLinares, il propose un réel-savoir faire sur tous types de produits.",
+    btn: "Voir le site",
+  },
+  {
+    src: "/img-exemple.png",
+    title: "Agence LLinares Immobilier 3",
+    text: "Créateur de la Maison LLinares, il propose un réel-savoir faire sur tous types de produits.",
+    btn: "Voir le site",
   },
 ];
 
@@ -44,32 +40,32 @@ const cardVariants = {
 const SectionHomeGrid = () => {
   return (
     <>
-      <h2 className=' text-pink-500 w-full py-16 text-center text-5xl sm:text-6xl'>
+      <h2 className='w-full py-16 text-center text-5xl text-pink-500 sm:text-6xl'>
         Nos modeles
       </h2>
       <Grid>
         {items.map((item, index) => (
           <motion.div
             key={item.title}
-            className='card card-compact overflow-hidden w-96 border-2 border-solid border-sky-200 text-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_2px_#08f,0_0_5px_#08f] bg-black '
+            className='card card-compact w-96 overflow-hidden border-2 border-solid border-sky-200 bg-black text-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_2px_#08f,0_0_5px_#08f]'
             variants={cardVariants}
-            initial="hidden"
-            animate="visible"
+            initial='hidden'
+            animate='visible'
             custom={index}
           >
-            <Link href={item.link}> 
-            <figure>
-              <img src={item.src} alt='' />
-            </figure>
-            <div className='card-body'>
-              <h2 className='card-title'>{item.title}</h2>
-              <p>{item.text}</p>
-              <div className='card-actions justify-start'>
-                <button className='text-md hover:text-sky-500 font-bold mr-4 mt-4 rounded-lg border-2 border-transparent bg- px-4 py-2 uppercase text-pink-500'>
-                  {item.btn}
-                </button>
+            <Link href={item.link}>
+              <figure>
+                <img src={item.src} alt='' />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>{item.title}</h2>
+                <p>{item.text}</p>
+                <div className='card-actions justify-start'>
+                  <button className='text-md bg- mr-4 mt-4 rounded-lg border-2 border-transparent px-4 py-2 font-bold uppercase text-pink-500 hover:text-sky-500'>
+                    {item.btn}
+                  </button>
+                </div>
               </div>
-            </div>
             </Link>
           </motion.div>
         ))}
