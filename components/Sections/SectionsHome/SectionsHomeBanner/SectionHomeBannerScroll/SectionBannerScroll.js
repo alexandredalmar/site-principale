242e6820e2a18ebd05eee428b813a5ae59dc1b55
GareFunkle <vincent.durret@gmail.com>
2024-11-03 17:43:14 +0100

    fix error on homepage with className

--- components/Sections/SectionsHome/SectionsHomeBanner/SectionHomeBannerScroll/SectionBannerScroll.js
+++ components/Sections/SectionsHome/SectionsHomeBanner/SectionHomeBannerScroll/SectionBannerScroll.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const SectionBannerScroll = () => {
   return (
-    <div class='left-50 absolute bottom-20 flex h-10 w-10 animate-bounce items-center justify-center rounded-full bg-white p-2 shadow-lg ring-1 ring-slate-900/5 dark:bg-slate-800 dark:ring-slate-200/20'>
+    <div className='left-50 absolute bottom-20 flex h-10 w-10 animate-bounce items-center justify-center rounded-full bg-white p-2 shadow-lg ring-1 ring-slate-900/5 dark:bg-slate-800 dark:ring-slate-200/20'>
       <ArrowDown className='h-12 w-12 stroke-pink-500' />
     </div>
   );
